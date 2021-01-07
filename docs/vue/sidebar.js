e9686b3fb71047ea9521fe7dd15c50024c001638
dongjian <931070423@qq.com>
2021-01-08 00:14:42 +0800

    md改名

--- docs/vue/sidebar.js
+++ docs/vue/sidebar.js
@@ -8,7 +8,7 @@ module.exports = [
         title: 'vue学习',
         collapsable: true,
         children: [
-            '/vue/vue2/README.md',
+            '/vue/vue2/template.md',
             '/vue/vue2/computed.md',
         ]
     }
